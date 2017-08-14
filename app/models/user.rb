447d744f69658c1637c2526cabf176f8929873ea
airi.iwama <airxxxx1209@gmail.com>
2017-08-14 10:56:16 +0000

    kadai-monolist

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,20 @@ class User < ApplicationRecord
   def want?(item)
     self.want_items.include?(item)
   end
+  
+  has_many :haves, class_name: "Have"
+  has_many :have_items, through: :haves, class_name: 'Item', source: :item
+
+  def have(item)
+    self.haves.find_or_create_by(item_id: item.id)
+  end
+
+  def unhave(item)
+    want = self.haves.find_by(item_id: item.id)
+    want.destroy if want
+  end
+
+  def have?(item)
+    self.have_items.include?(item)
+  end
 end
